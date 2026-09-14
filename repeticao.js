dbf301ae5ecbf6861fb4a8bacee75b163138b8fa
Gabriel <gabriel.bocchino@aluno.senai.br>
2026-09-14 17:21:05 -0300

    Até o nivel 3 do intermediario

---
+++ repeticao.js
@@ -0,0 +1,47 @@
+// for(let i = 1; i <= 10; i++) {
+//   console.log(i);
+// };
+
+
+// let numero = Number(prompt("Digite um número:"));
+
+// for(let i = 1; i <= 10; i++) {
+//   console.log(`${numero} x ${i} = ${numero * i}`);
+// };
+
+
+// let natural = Number(prompt("Digite um número natural:"));
+// let soma = 0;
+
+// for(let i = 1; i <= natural; i++) {
+//   soma += i;
+// }
+
+// console.log(`A soma de 1 até ${natural} é ${soma}`);
+
+
+// for(let i = 2; i <= 50; i += 2) {
+//   console.log(i);
+// };
+
+
+// let numero = Math.floor(Math.random() * 100) + 1;
+// let palpite = Number(prompt("Adivinhe o número (entre 1 e 100):"));
+
+// while (palpite !== numero) {
+//   if (palpite < numero) {
+//     palpite = Number(prompt("O número correto é MAIOR! Tente novamente:"));
+//   } else {
+//     palpite = Number(prompt("O número correto é MENOR! Tente novamente:"));
+//   }
+// }
+
+// alert(`Parabéns! Você acertou, o número era ${numero}.`);
+
+
+// let numero = Number(prompt("Digite um número:"));
+
+// while (numero >= 0) {
+//     console.log(numero);
+//     numero --;
+// }
